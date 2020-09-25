f890a7d2026e0d069889afc453d62f6a515906e4
francepants <franceline90@gmail.com>
2020-09-25 18:08:48 -0500

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React, { Component } from 'react'
+
+export class Keypad extends Component {
+    // tickle = () => {
+    //     console.log('Tee hee!');
+    //   }
+    handleOnKeyUp = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="password" onKeyUp={this.handleOnKeyUp}/>
+            </div>
+        )
+    }
+}
+
+export default Keypad
